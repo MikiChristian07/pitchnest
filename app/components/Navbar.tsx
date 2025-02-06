eb8b44ce289d7e15f2397fe668ecd189ae03f7ff
Onovo Christian <onovochristian19@gmail.com>
2025-02-06 13:26:51 +0100

    Got the next-auth to work by splitting the server component with a seperate client component that handle the event

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { auth } from '@/auth';
+import NavbarClient from './NavbarClient';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-        <nav className="flex justify-between items-center">
-            <Link href="/">
-                <img src="/logo.jpg" alt="logo" width={144} height={30}/>
-            </Link>
-
-            <div className="flex items-center gap-5">
-                
-            </div>
-        </nav>
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.jpg" alt="logo" width={144} height={30} />
+        </Link>
+        <NavbarClient session={session} />
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
