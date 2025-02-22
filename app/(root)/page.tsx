eb78351ee99b1495a0f07ea8d9cc1b7cf68f456a
Onovo Christian <onovochristian19@gmail.com>
2025-02-22 21:32:15 +0100

    Implemneted logic for search query results

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{query?: string }>
 }) {
 
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id : 1 },
+    description: 'This is a sample description',
+    image: 'https://images.unsplash.com/photo-1559136555-9303baea8ebd?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3RhcnR1cHxlbnwwfHwwfHx8MA%3D%3D',
+    category: 'Blockchain',
+    title: 'Decenti-Firm',
+  }]
 
   return (
     <>
@@ -16,6 +27,24 @@ export default async function Home({ searchParams }: {
         </p>
         <SearchForm query={query}/>
         
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card-grid">
+          {
+            posts?.length > 0 ? (
+              posts.map((post : StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+
+            ) :
+              <p className="no-results">No results found</p>
+          }
+        </ul>
       </section>
 
     </>
