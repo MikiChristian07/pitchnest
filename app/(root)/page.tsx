7a3c364c821cbd9ff99c47854623e213056df7c8
Onovo Christian <onovochristian19@gmail.com>
2025-03-12 12:53:05 +0100

    Implemented live reloading of the cache with live-context-api

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
-export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
-  const params = await searchParams; // Wait for the search params
-  const query = params?.query || "";
+export default async function Home({ 
+  searchParams,
+}: { 
+  searchParams: Promise<{ query?: string }> 
+}) {
+  const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   console.log(JSON.stringify(posts));
 
@@ -36,6 +39,8 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
