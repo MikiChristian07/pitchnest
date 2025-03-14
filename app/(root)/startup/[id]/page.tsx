37fd5526152f74a9e7b2192ea81c5a733f6e48a1
Onovo Christian <onovochristian19@gmail.com>
2025-03-14 06:15:25 +0100

    Minor changes for now

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -14,6 +14,10 @@ const Page = async ({ params }: { params: { id: string } }) => {
 
     return (
         <>
+
+            <section>
+                
+            </section>
             <h1 className='text-3xl'>{post.title}</h1>
         </>
     );
