6dc5820855c6475a7c8ebb4f9a8276130b2de486
Onovo Christian <onovochristian19@gmail.com>
2025-03-22 19:11:16 +0100

    Create new user session and pesris user (author) to sanity db

--- auth.ts
+++ auth.ts
@@ -1,6 +1,52 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries"
+import { client } from "./sanity/lib/client"
+import { writeClient } from "./sanity/lib/write-client"
+import { User } from "lucide-react"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
-})
+  callbacks: {
+    async signIn({
+      user: { name, email, image },
+      profile: { id, login, bio }
+    }) {
+      const exisitingUser = await client.fetch( AUTHOR_BY_GITHUB_ID_QUERY, { 
+        id: id
+      });
+
+      if (!exisitingUser) {
+        await writeClient.create({
+          _type: 'author',
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || '',
+        });
+      }
+
+      return true;
+    },
+
+    async jwt ({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id: profile?.id,
+        })
+
+        token.id = user?._id
+      }
+
+      return token;
+    },
+
+    async session ({ session, token }) {
+      Object.assign(session, { id: token.id });
+
+      return session;
+    }
+  }
+});
