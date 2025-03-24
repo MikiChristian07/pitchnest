59e5226e5eb428fa2b831d591d3d20a3e9c39570
Onovo Christian <onovochristian19@gmail.com>
2025-03-24 11:06:43 +0100

    Imported markup field for pitch

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -2,9 +2,16 @@
 
 import { useState } from "react";
 import { Textarea } from "./ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "./ui/button";
+import { Send } from "lucide-react";
 
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string, string>>({});
+
+    const [pitch, setPitch] = useState("");
+
+    const isPending = false;
 
     return (
         <form action={() => {}} className="startup-form max-w-md mx-auto space-y-6">
@@ -35,54 +42,90 @@ const StartupForm = () => {
                     Description
                 </label>
                 <Textarea
-                    id="title"
-                    name="title"
-                    className="startup-form_input mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm"
+                    id="description"
+                    name="description"
+                    className="startup-form_textarea mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm"
                     required
-                    placeholder="Startup Title"
+                    placeholder="Startup Description"
                 />
-                {errors.title && (
-                    <p className="text-green-500 text-xs mt-1">{errors.title}</p>
+                {errors.description && (
+                    <p className="text-green-500 text-xs mt-1">{errors.description}</p>
                 )}
             </div>
 
             <div className="form-group">
                 <label
-                    htmlFor="title"
+                    htmlFor="category"
                     className=" startup-form_label block text-sm font-medium text-gray-700"
                 >
-                    Title
+                    Category
                 </label>
                 <input
-                    id="title"
-                    name="title"
+                    id="category"
+                    name="category"
                     className="startup-form_input mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm"
                     required
-                    placeholder="Startup Title"
+                    placeholder="Startup Category (Cars, Agriculture, Health)"
                 />
-                {errors.title && (
-                    <p className="text-green-500 text-xs mt-1">{errors.title}</p>
+                {errors.category && (
+                    <p className="text-green-500 text-xs mt-1">{errors.category}</p>
                 )}
             </div>
 
             <div className="form-group">
                 <label
-                    htmlFor="title"
+                    htmlFor="link"
                     className=" startup-form_label block text-sm font-medium text-gray-700"
                 >
-                    Title
+                    Image URL
                 </label>
                 <input
-                    id="title"
-                    name="title"
+                    id="link"
+                    name="link"
                     className="startup-form_input mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm"
                     required
-                    placeholder="Startup Title"
+                    placeholder="Startup Image URL"
                 />
-                {errors.title && (
-                    <p className="text-green-500 text-xs mt-1">{errors.title}</p>
+                {errors.link && (
+                    <p className="text-green-500 text-xs mt-1">{errors.link}</p>
                 )}
             </div>
+
+            <div className="form-group" data-color-mode="light">
+                <label
+                    htmlFor="pitch"
+                    className=" startup-form_label block text-sm font-medium text-gray-700"
+                >
+                    Pitch
+                </label>
+                <MDEditor
+                    value={pitch}
+                    onChange={(value) => setPitch(value as string)}
+                    className="startup-form_textarea mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm" 
+                    id="pitch"
+                    preview="edit"
+                    height={300}
+                    style={{borderRadius: 20, overflow: "hidden"}}
+                    textareaProps={{
+                        placeholder: "Tell us about you idea and what it will solve"
+                    }}
+                    previewOptions={{
+                        disallowedElements: ["style"]
+                    }}
+                />
+                {errors.link && (
+                    <p className="text-green-500 text-xs mt-1">{errors.link}</p>
+                )}
+            </div>
+
+            <Button 
+                type="submit" 
+                className="startup-form_btn text-white"
+                disabled={isPending}
+            >
+                {isPending ? "Submitting..." : "Submit"}
+                <Send className="size-6 ml-2" />
+            </Button>
         </form>
     );
 };
