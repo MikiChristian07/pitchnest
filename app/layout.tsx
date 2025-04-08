dbd0c1ad42ce289eaf4985bc8d39fe0678d0e3fe
Onovo Christian <onovochristian19@gmail.com>
2025-04-08 10:01:56 +0100

    Finished validation finctionality for valdation pitch

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local"; // Import localFont
 import "./globals.css";
 import 'easymde/dist/easymde.min.css';
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
   src: [
@@ -64,6 +65,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={workSans.variable}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,24 +1,95 @@
 "use client";
 
-import { useState } from "react";
+import { useActionState, useState } from "react";
+import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import MDEditor from "@uiw/react-md-editor";
 import { Button } from "./ui/button";
 import { Send } from "lucide-react";
+import { formSchema } from "@/lib/validation";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string, string>>({});
-
     const [pitch, setPitch] = useState("");
+    const { toast } = useToast();
+    const router = useRouter(); 
 
-    const isPending = false;
+    const handleFormSubmit = async (prevState: any, formData: FormData) => {
+        try {
+            const formValues = {
+                title: formData.get("title") as string,
+                description: formData.get("description") as string,
+                category: formData.get("category") as string,
+                link: formData.get("link") as string,
+                pitch,
+            };
+
+            // Validate the form values
+            await formSchema.parseAsync(formValues);
+
+            console.log(formValues);
+
+            // Reset errors if validation passes
+            setErrors({});
+
+            // Submit the form (e.g., call an API)
+            // const result = await createIdea(prevState, formData, pitch);
+            // console.log(result);
+
+            // if (result.status === "SUCCESS") {
+            //     toast({
+            //         title: "Success",
+            //         description: "Your startup idea has been submitted successfully.",
+            //         variant: "default",
+            //     });
+
+            //     router.push(`/startup/${result.id}`); // Redirect to the home page after successful submission
+            // }
+
+            // return result;
+            
+        } catch (error: any) {
+            // Catch validation errors and update the errors state
+            if (error.name === "ZodError") {
+                const validationErrors: Record<string, string> = {};
+                error.errors.forEach((err: any) => {
+                    validationErrors[err.path[0]] = err.message;
+                });
+                setErrors(validationErrors);
+                toast({
+                    title: "Validation Error",
+                    description: "Please check the form fields.",
+                    variant: "destructive",
+                });
+            }
+
+            toast({
+                title: "Error",
+                description: "An error occurred while submitting the form",
+                variant: "destructive",
+            });
+
+            return {
+                ...prevState,
+                error: "An error occurred while submitting the form.",
+                status: "ERROR",
+            }
+        }
+    };
+
+    const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+        error: "",
+        status: "INITIAL",
+    });
 
     return (
-        <form action={() => {}} className="startup-form max-w-md mx-auto space-y-6">
+        <form action={formAction} className="startup-form max-w-md mx-auto space-y-6">
             <div className="form-group">
                 <label
                     htmlFor="title"
-                    className=" startup-form_label block text-sm font-medium text-gray-700"
+                    className="startup-form_label block text-sm font-medium text-gray-700"
                 >
                     Title
                 </label>
@@ -30,14 +101,14 @@ const StartupForm = () => {
                     placeholder="Startup Title"
                 />
                 {errors.title && (
-                    <p className="text-green-500 text-xs mt-1">{errors.title}</p>
+                    <p className="text-red-500 text-xs mt-1">{errors.title}</p>
                 )}
             </div>
 
             <div className="form-group">
                 <label
                     htmlFor="description"
-                    className=" startup-form_label block text-sm font-medium text-gray-700"
+                    className="startup-form_label block text-sm font-medium text-gray-700"
                 >
                     Description
                 </label>
@@ -49,14 +120,14 @@ const StartupForm = () => {
                     placeholder="Startup Description"
                 />
                 {errors.description && (
-                    <p className="text-green-500 text-xs mt-1">{errors.description}</p>
+                    <p className="text-red-500 text-xs mt-1">{errors.description}</p>
                 )}
             </div>
 
             <div className="form-group">
                 <label
                     htmlFor="category"
-                    className=" startup-form_label block text-sm font-medium text-gray-700"
+                    className="startup-form_label block text-sm font-medium text-gray-700"
                 >
                     Category
                 </label>
@@ -68,14 +139,14 @@ const StartupForm = () => {
                     placeholder="Startup Category (Cars, Agriculture, Health)"
                 />
                 {errors.category && (
-                    <p className="text-green-500 text-xs mt-1">{errors.category}</p>
+                    <p className="text-red-500 text-xs mt-1">{errors.category}</p>
                 )}
             </div>
 
             <div className="form-group">
                 <label
                     htmlFor="link"
-                    className=" startup-form_label block text-sm font-medium text-gray-700"
+                    className="startup-form_label block text-sm font-medium text-gray-700"
                 >
                     Image URL
                 </label>
@@ -87,39 +158,39 @@ const StartupForm = () => {
                     placeholder="Startup Image URL"
                 />
                 {errors.link && (
-                    <p className="text-green-500 text-xs mt-1">{errors.link}</p>
+                    <p className="text-red-500 text-xs mt-1">{errors.link}</p>
                 )}
             </div>
 
             <div className="form-group" data-color-mode="light">
                 <label
                     htmlFor="pitch"
-                    className=" startup-form_label block text-sm font-medium text-gray-700"
+                    className="startup-form_label block text-sm font-medium text-gray-700"
                 >
                     Pitch
                 </label>
                 <MDEditor
                     value={pitch}
                     onChange={(value) => setPitch(value as string)}
-                    className="startup-form_textarea mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm" 
+                    className="startup-form_textarea mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm"
                     id="pitch"
                     preview="edit"
                     height={300}
-                    style={{borderRadius: 20, overflow: "hidden"}}
+                    style={{ borderRadius: 20, overflow: "hidden" }}
                     textareaProps={{
-                        placeholder: "Tell us about you idea and what it will solve"
+                        placeholder: "Tell us about your idea and what it will solve",
                     }}
                     previewOptions={{
-                        disallowedElements: ["style"]
+                        disallowedElements: ["style"],
                     }}
                 />
-                {errors.link && (
-                    <p className="text-green-500 text-xs mt-1">{errors.link}</p>
+                {errors.pitch && (
+                    <p className="text-red-500 text-xs mt-1">{errors.pitch}</p>
                 )}
             </div>
 
-            <Button 
-                type="submit" 
+            <Button
+                type="submit"
                 className="startup-form_btn text-white"
                 disabled={isPending}
             >
